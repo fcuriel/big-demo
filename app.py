d61d9533f3e9a1577f87907bd97518d812a9a35c
Damian <damian1489@gmail.com>
2018-02-12 00:45:13 -0600

    HTML add

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template 
 from redis import Redis, RedisError
 import os
 import socket
@@ -10,15 +10,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+	return render_template("index.html")
+    
+		   
+		  
+    #return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
